37ec88f7f8167ff961752f68a528e71fc5fa6657
Kofotsmordarn <steen.rasmus.95@gmail.com>
2018-03-17 21:12:03 +0100

    animated background

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import * as PIXI from 'pixi.js'
 
-import pixieSpine from './assets/Pixie.js'
-
 import bgTile from './assets/iP4_BGtile.jpg'
 import ground from './assets/iP4_ground.png'
-
-console.log(pixieSpine)
 
 var app = new PIXI.Application()
 document.body.appendChild(app.view)
@@ -13,7 +9,7 @@ document.body.appendChild(app.view)
 app.stop()
 
 // load spine data
-PIXI.loader.add('pixie', pixieSpine).load(onAssetsLoaded)
+PIXI.loader.load(onAssetsLoaded)
 
 var postition = 0
 var background
@@ -32,29 +28,6 @@ function onAssetsLoaded(loader, res) {
   foreground.y = foreground2.y = 640 - foreground2.height
 
   app.stage.addChild(background, background2, foreground, foreground2)
-
-  var pixie = new PIXI.spine.Spine(res.pixie.spineData)
-
-  var scale = 0.3
-
-  pixie.x = 1024 / 3
-  pixie.y = 500
-
-  pixie.scale.x = pixie.scale.y = scale
-
-  app.stage.addChild(pixie)
-
-  pixie.stateData.setMix('running', 'jump', 0.2)
-  pixie.stateData.setMix('jump', 'running', 0.4)
-
-  pixie.state.setAnimation(0, 'running', true)
-
-  app.stage.on('pointerdown', onTouchStart)
-
-  function onTouchStart() {
-    pixie.state.setAnimation(0, 'jump', false)
-    pixie.state.addAnimation(0, 'running', true, 0)
-  }
 
   app.start()
 }
